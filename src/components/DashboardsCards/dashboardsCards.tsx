1a78fdc287154b5b0ab4c20160819ef48c3562d6
Felipe Oliveira <lipefan0@gmail.com>
2024-06-12 15:19:45 -0300

    Alterações de layout e cadastro de usuários

--- src/components/DashboardsCards/dashboardsCards.tsx
+++ src/components/DashboardsCards/dashboardsCards.tsx
@@ -44,8 +44,16 @@ export default function DashboardCard(props: Props) {
   return (
     <div className="flex content-center justify-center m-4">
       <Card className="w-[300px] h-[320px] flex flex-col justify-between items-center">
-        <CardHeader className="flex flex-col items-center w-full">
-          <Image alt={props.altImg} src={props.img} width={250} height={90} />
+        <CardHeader className="flex flex-col items-center w-full ">
+          <div className="flex h-24">
+            <Image
+              alt={props.altImg}
+              src={props.img}
+              width={250}
+              height={90}
+              className="rounded-full w-24"
+            />
+          </div>
           <CardTitle>{props.title}</CardTitle>
         </CardHeader>
         <CardFooter className="flex justify-evenly w-full">
